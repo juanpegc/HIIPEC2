229d756431cdfa4196f7e8cbb0571927daa9c34b
juanpegc <68005520+juanpegc@users.noreply.github.com>
2023-05-10 00:59:49 +0200

    first commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,7 @@
+import * as bootstrap from "bootstrap";
+
+import { Splide } from "@splidejs/splide";
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Splide("#image-carousel").mount();
+});
